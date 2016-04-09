1a3787daa6f5e712cc751bb4505078a984d1c3d6
nava500 <nava500@outlook.com>
2016-04-09 23:23:01 +0300

    update prot for heroku

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 var middleware = require('./middleware.js');
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 app.use(middleware.logger);
 app.use(express.static(__dirname+'/public'));
